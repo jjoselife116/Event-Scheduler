2a8a92354b61d410fa66c9bb6fff97f423406e63
TheVillageFool <joshua.vitullo@gmail.com>
2018-05-15 14:48:12 -0500

    Add files via upload
    Changes to single table in "testindex2.html", "teststyle.css", and "javascript.js".

--- addEmp.js
+++ addEmp.js
@@ -1,14 +1,16 @@
 function create_employee(form){
+/*
+  let employee = new Object();
+  employee.EmployeeName         = form.empName.value;
+  employee.TaskDescripts        = form.tskdes.value;
+  employee.TaskStartDate       = form.tskstart.value;
+  employee.TaskEndDate         = form.tskend.value;
+  employee.TaskStatus           = form.tskStatus.value;
+*/
 
-let LocalData = localStorage.getItem(form.empName.value);
-if(LocalData){
 
-}
-
-
-// create a employee object to store the employee's name and taks info
 let employee = {
-  "Employee Name"                 : form.empName.value,
+  "Employee Name"     : form.empName.value,
   "Task": [{
           "Task Name"             : form.tskname.value,
           "Task Descripts"        : form.tskdes.value,
@@ -18,11 +20,21 @@ let employee = {
         }]
       };
 
-// set the local storage to the new employee array
-localStorage.setItem(form.empName.value, JSON.stringify(employee));
+//let obj = JSON.parse(employee);
 
+/*
+obj['Task'].push({
+  "Task Name"             : form.tskname.value,
+  "Task Descripts"        : form.tskdes.value,
+  "Task Start Date"       : form.tskstart.value,
+  "Task End Date"         : form.tskend.value,
+  "Task Status"           : form.tskStatus.value,});
+*/
 
+  let employeeJson = JSON.stringify(employee,0,4);
+  console.log(employeeJson);
 
+  localStorage.setItem("Employee",employeeJson);
 
 
 }
