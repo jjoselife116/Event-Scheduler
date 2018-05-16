7d9452d423fda2b582b46d32b622d237c614fb50
Ryan Connell <ryan.connell88@gmail.com>
2018-05-16 15:47:07 -0500

    Add files via upload

--- addEmp.js
+++ addEmp.js
@@ -9,13 +9,8 @@ if(form.empName.value in localStorage){
   // create a employee object to store the employee's name and taks info
   let employee = {
     "Employee Name"                 : form.empName.value,
-    "Task": [{
-            "Task Name"             : form.tskname.value,
-            "Task Descripts"        : form.tskdes.value,
-            "Task Start Date"       : form.tskstart.value,
-            "Task End Date"         : form.tskend.value,
-            "Task Status"           : form.tskStatus.value,
-          }]
+    "Employee Id"                   : form.empId.value,
+    
         };
 
   // set the local storage to the new employee array
--- displayTask.js
+++ displayTask.js
@@ -1,39 +1,63 @@
 
-  let taskData = JSON.parse(localStorage.getItem("Bill nye"));
-  //console.log(taskData.Task[0]);
-  let task = taskData.Task[0]['Task Name'];
-
-
-
-
-   var typeofKey = null;
-   let i =0;
-   let employee;
-
-
 $(document).ready(function(){
 
+let typeofKey;
+let empArry = [];
+let employee;
 
-  for (var key in localStorage) {
-    typeofKey = (typeof localStorage[key]);
-    if(typeofKey == 'string' || typeofKey instanceof String ){
-    employee = JSON.parse(localStorage.getItem(key));
-    console.log(employee.Task[0]);
+      //cols of the employee names
+      for (var i = 0; i < localStorage.length; i++){
+
+        empArry.push(localStorage.key(i))
+
+
+
+      }
+    //  console.log(empArry.toString());
+    // loop through the local storage and pull the data
+for(let j = 0; j < empArry.length; j++){
+
+  employee = JSON.parse(localStorage.getItem(empArry[j]));
+  // If employee has any task
+  if("Task" in employee){
+    // while employee has task in his array
+  for(let taskIndex = 0; taskIndex < employee.Task.length; taskIndex++){
+      //console.log(employee['Employee Name']);
+      //console.log(employee.Task[taskIndex]["Task Name"])
+      //
+      if($("#wholeTable #emp0-1").text() == employee['Employee Name']){
+        $('#1-1').html(employee.Task[taskIndex]["Task Name"]);
+
+      }
 
 }
+
+
+
+    }
+
 }
-     $("#2-0").append("<span style='display:block; background-color:red; width: 435px;'>Deb Stop eating bagels</span>");
-
-     $("#4-3").append("<span style='display:block; background-color:blue; width: 435px;'>hhhhhhh </span>");
-     $("#4-3 > span").html(task);
 
 
 
-      i++;
-      //console.log(key, typeofKey);
 
 
+
+
+
+
+
+//$('#4-2').html(employee.Task[0]["Task Name"]);
 
 
 
 });
+
+
+
+
+
+
+
+
+      //console.log(key, typeofKey);
