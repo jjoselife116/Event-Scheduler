948e801e153c94e0950dd789b4bbeb1a0a7949ce
Josh <jvirgiljoseph@yahoo.com>
2018-05-21 14:57:05 -0500

    Add files via upload

---
+++ assets/js/addTask.js
@@ -0,0 +1,34 @@
+
+
+function submit_json(form){
+
+//console.log(form.emplName.value);
+
+// pull from the local storage
+//let employee = JSON.parse(localStorage.getItem(form.emplName.value);
+let employee = localStorage.getItem($('#empDropDown').val());
+
+employee = JSON.parse(employee);
+
+
+// add task to the end of the task array for chosen employee
+if('Task' in employee){
+employee.Task.push({
+  "Task Name"             : form.tskname.value,
+  "Task Descripts"        : form.tskdes.value,
+  "Task Start Date"       : form.tskstart.value,
+  "Task End Date"         : form.tskend.value,
+  "Task Status"           : form.tskStatus.value,
+});
+}
+
+// overwrite the employee JSON in the local storage with the newly updated task array
+localStorage.setItem(form.emplName.value, JSON.stringify(employee));
+
+
+
+
+
+//console.log(employee);
+
+}
---
+++ assets/js/displayTask.js
@@ -0,0 +1,44 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+$(document).ready(function(){
+  let typeofKey;
+  let empArry = [];
+  let employee;
+  let myTable = document.getElementById('wholeTable');
+  let colFill = false;
+  let color = null;
+        //cols of the employee names
+  for (var i = 0; i < localStorage.length; i++){
+      empArry.push(localStorage.key(i))
+  }
+  // loop through the local storage and pull the data
+  for(let j = 0; j < empArry.length; j++){
+    color = getRandomColor();
+    employee = JSON.parse(localStorage.getItem(empArry[j]));
+    // If employee has any task
+    if("Task" in employee){
+      // while employee has task in his array
+      for(let taskIndex = 0; taskIndex < employee.Task.length; taskIndex++){
+         for(let k = 1; k < myTable.rows[0].cells.length; k++) {
+           if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task Start Date"]) {
+             colFill = true;
+           }
+           if(colFill == true) {
+             myTable.rows[j+1].cells[k].innerHTML += '<span style="background-color:' + color + '">' + employee.Task[taskIndex]["Task Name"] + '</br></span>';
+           }
+           if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task End Date"]) {
+             colFill = false;
+           }
+         }
+       }
+     }
+   }
+});
