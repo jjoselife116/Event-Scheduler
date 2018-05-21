5443dd0efe3fe28d2aa0f7416c69b978c517297e
Ryan Connell <ryan.connell88@gmail.com>
2018-05-21 15:59:33 -0500

    updates

--- assets/js/displayTask.js
+++ assets/js/displayTask.js
@@ -15,11 +15,15 @@ $(document).ready(function(){
   let myTable = document.getElementById('wholeTable');
   let colFill = false;
   let color = null;
+  let taskStart=0;
+  let taskEnd=0;
+  let taskMid = 0;
         //cols of the employee names
   for (var i = 0; i < localStorage.length; i++){
       empArry.push(localStorage.key(i))
   }
   // loop through the local storage and pull the data
+
   for(let j = 0; j < empArry.length; j++){
     color = getRandomColor();
     employee = JSON.parse(localStorage.getItem(empArry[j]));
@@ -28,15 +32,27 @@ $(document).ready(function(){
       // while employee has task in his array
       for(let taskIndex = 0; taskIndex < employee.Task.length; taskIndex++){
          for(let k = 1; k < myTable.rows[0].cells.length; k++) {
+
            if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task Start Date"]) {
              colFill = true;
-           }
-           if(colFill == true) {
-             myTable.rows[j+1].cells[k].innerHTML += '<span style="background-color:' + color + '">' + employee.Task[taskIndex]["Task Name"] + '</br></span>';
+             taskStart = myTable.rows[0].cells[k].cellIndex;
            }
            if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task End Date"]) {
+             taskEnd = myTable.rows[0].cells[k].cellIndex;
+
+             taskMid = taskEnd - taskStart;
+             
              colFill = false;
            }
+           if(colFill == true) {
+             // add task name to the cell
+
+
+             myTable.rows[j+1].cells[k].innerHTML += '<div style="background-color:' + color + '">' + employee.Task[taskIndex]["Task Name"] + '</div>';
+           }
+
+
+
          }
        }
      }
