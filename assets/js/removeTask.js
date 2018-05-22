850ecda502c7c02a11842ff3c67f93e724599695
Josh <jvjoseph@uh.edu>
2018-05-22 13:51:52 -0500

    form and css changes 2

---
+++ assets/js/removeTask.js
@@ -0,0 +1,20 @@
+function remove_task(form){
+
+
+    let employee = localStorage.getItem($('#removeDropDown').val());
+
+    employee = JSON.parse(employee);
+
+
+
+  let indexToRemove = $("#taskDropDown").prop('selectedIndex')
+  var removed = employee.Task.splice(indexToRemove, 1);
+
+
+
+alert($(removeDropDown).val());
+  // overwrite the employee JSON in the local storage with the newly updated task array
+  localStorage.setItem($(removeDropDown).val(), JSON.stringify(employee));
+
+
+}
