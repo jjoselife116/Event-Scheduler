914a97a9bf3962ecf2861b91bb8e78d9ddead5cf
Ryan Connell <ryan.connell88@gmail.com>
2018-05-31 10:38:53 -0500

    text changes with background

--- assets/js/displayTask.js
+++ assets/js/displayTask.js
@@ -18,36 +18,55 @@ $(document).ready(function(){
     employee = JSON.parse(localStorage.getItem(empArry[j]));
     // If employee has any task
     if("Task" in employee){
+
       // while employee has task in his array
       for(let taskIndex = 0; taskIndex < employee.Task.length; taskIndex++){
+            taskID++;
+
          for(let k = 1; k < myTable.rows[0].cells.length; k++) {
            if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task Start Date"]) {
              firstK = k;
              colFill = true;
 
+
            }
            if(colFill == true) {
 
-            myTable.rows[j+1].cells[k].setAttribute("class", "TaskID-"+taskID);
-
-
             myTable.rows[j+1].cells[k].innerHTML += '<div id="taskID-' + taskID + '" style="background-color:' + employee["Employee Color"] + '; border-style: hidden hidden solid hidden; border-width: 3px;border-color: white ">' + employee.Task[taskIndex]["Task Name"] + '</br></div>';
+            var rgb = employee["Employee Color"];
+            var colr;
+            if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(rgb)){
+                colr= rgb.substring(1).split('');
+                if(colr.length== 3){
+                    colr= [colr[0], colr[0], colr[1], colr[1], colr[2], colr[2]];
+                }
+                colr= '0x'+colr.join('');
+                var rgb =[(colr>>16)&255, (colr>>8)&255, colr&255].join(',')+',1'
+              }
+            bgroundColor = Math.round(((parseInt(rgb[0]) * 299) +
+                        (parseInt(rgb[1]) * 587) +
+                        (parseInt(rgb[2]) * 114)) / 2);
 
+              var newTextColor = (bgroundColor > 125) ? 'black' : 'white';
+
+              $('#taskID-' + taskID).css('color', newTextColor);
            }
+
 
            if(myTable.rows[0].cells[k].innerHTML == employee.Task[taskIndex]["Task End Date"]) {
              colFill = false;
              lastK = k;
             }
            mid = Math.floor((lastK + firstK )/ 2);
-             taskID++;
+                 taskID++;
          }
 
 
 
 
+
        }
-       taskID=1;
+
      }
    }
 });
