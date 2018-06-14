af292c324653769122f938e178d8eb55b66c40cf
Ryan Connell <ryan.connell88@gmail.com>
2018-06-14 12:04:40 -0500

    Delete removeEmp.js

--- assets/js/removeEmp.js
+++
@@ -1,9 +0,0 @@
-function remove_Emp(form){
-    // pull employee to remove from dropdown list
-    let employeeToRemove = localStorage.getItem($('#removeEmpDropDown').val());
-
-    employeeToRemove = JSON.parse(employeeToRemove);
-    // Remove selected employee from the local storage
-    localStorage.removeItem(employeeToRemove["Employee Name"]);
-
-}
